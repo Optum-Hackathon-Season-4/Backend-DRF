80f02aefc7882638ab710d8cab3973987dab98d9
dragonman164 <sanidhiyafirefox123@gmail.com>
2022-10-28 15:46:17 +0530

    integration of hospital addition

--- connecto/settings.py
+++ connecto/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 AUTH_USER_MODEL = 'system.User'
@@ -63,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'connecto.urls'
